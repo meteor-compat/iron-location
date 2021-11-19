175936a5b7a879e5060390655f9f3c4d7ffe107e
filipenevola <filipemkv@gmail.com>
2021-11-19 10:34:49 -0400

    Published iron:location@1.1.0.

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: 'iron:location',
   summary: 'Reactive urls that work in IE8/9 and modern browsers.',
-  version: '1.0.11',
+  version: '1.1.0',
   git: 'https://github.com/eventedmind/iron-location.git'
 });
 
-Package.on_use(function (api) {
+Package.onUse(function (api) {
   api.versionsFrom('METEOR@0.9.2');
 
   api.use('underscore');
   api.use('tracker');
-  api.use('jquery');
+  api.use('jquery@1.11.9 || 3.0.0');
 
   api.use('iron:core@1.0.11');
   api.imply('iron:core');
@@ -19,17 +19,17 @@ Package.on_use(function (api) {
 
   api.use('appcache', {weak: true});
 
-  api.add_files('lib/utils.js', 'client');
-  api.add_files('lib/state.js', 'client');
-  api.add_files('lib/location.js', 'client');
+  api.addFiles('lib/utils.js', 'client');
+  api.addFiles('lib/state.js', 'client');
+  api.addFiles('lib/location.js', 'client');
 
   api.export(['urlToHashStyle', 'urlFromHashStyle'], 'client', {testOnly: true});
 });
 
-Package.on_test(function (api) {
+Package.onTest(function (api) {
   api.use('iron:location');
   api.use('tinytest');
   api.use('test-helpers');
 
-  api.add_files('test/location_test.js', 'client');
+  api.addFiles('test/location_test.js', 'client');
 });
